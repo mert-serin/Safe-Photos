9c8b7f84f10a668bd0ec6883b664c9352c84a6be
Mert Serin <mert-serin@outlook.com>
2016-10-31 12:31:33 +0300

    initial commit

---
+++ Keep Safe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Keep Safe
+//
+//  Created by Mert Serin on 31/10/2016.
+//  Copyright © 2016 Mert Serin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
